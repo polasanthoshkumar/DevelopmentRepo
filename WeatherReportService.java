bcbf8c3f1adb2fcd3ff9232fc1ebf6c2108d589e
polasanthoshkumar <51431955+polasanthoshkumar@users.noreply.github.com>
2019-06-07 15:49:55 +0800

    Delete WeatherReportService.java

--- WeatherReportService.java
+++
@@ -1,9 +0,0 @@
-package com.pactero.weatherreport.service;
-
-import com.pactero.weatherreport.model.City;
-
-public interface WeatherReportService {
-	
-	public City getWeatherReportForGivenCity(City city);
-
-}
